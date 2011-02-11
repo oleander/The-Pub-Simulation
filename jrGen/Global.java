58988f8d6c1a272b41b2797eed956c6e7ffad6c1
Arash Rouhani <miffoljud@gmail.com>
2011-02-11 17:42:00 +0100

    Added all beverage and ingredient constants

--- jrGen/Global.java
+++ jrGen/Global.java
@@ -20,6 +20,21 @@ public class Global extends java.lang.Object {
     }
     public static Door door;
     public static int tableCapacities = 10;
+    private static int BEER_DRINKING_TIME = 100;
+    private static int CAPPUCINO_DRINKING_TIME = 100;
+    private static int CHOCLATE_DRINKING_TIME = 100;
+    public static Item beerTap = new Item("Beer tap");
+    public static Item coffee = new Item("Coffee");
+    public static Item milk = new Item("Milk");
+    public static Item cholcatePowder = new Item("Cholcate Powder");
+    public static BeverageWare glass = new BeverageWare("Glass", 2);
+    public static BeverageWare cup = new BeverageWare("Cup", 1);
+    private static Item[] beerIngredients = {};
+    private static Item[] cappucinoIngredients = {};
+    private static Item[] choclateIngredients = {};
+    public static Beverage beer = new Beverage("Beer", BEER_DRINKING_TIME, glass, beerIngredients);
+    public static Beverage cappucino = new Beverage("Cappucino", CAPPUCINO_DRINKING_TIME, cup, cappucinoIngredients);
+    public static Beverage choclate = new Beverage("Choclate", CHOCLATE_DRINKING_TIME, cup, choclateIngredients);
     protected boolean JRcalled = false;
     protected JRGlobal jrresref;
     public Object JRgetjrresref()
