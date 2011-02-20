4ed36ccb2fce0bc5a9de736c6de9ab24cdd2503f
Arash Rouhani <miffoljud@gmail.com>
2011-02-20 12:44:27 +0100

    Added statistics field in Customer

--- jrGen/Item.java
+++ jrGen/Item.java
@@ -29,47 +29,6 @@ public class Item extends java.lang.Object {
         this.name = name;
         JRprocess();
     }
-    
-    @Override()
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            // Return
-            return (false);
-            // End Return
-
-        }
-        if (getClass() != obj.getClass()) {
-            // Return
-            return (false);
-            // End Return
-
-        }
-        final Item other = ((Item)((Item)obj));
-        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
-            // Return
-            return (false);
-            // End Return
-
-        }
-        // Return
-        return (true);
-        // End Return
-
-    }
-    
-    @Override()
-    public int hashCode() {
-        int hash = 5;
-        // Begin Expr2
-        hash = 79 * hash + (this.name != null ? this.name.hashCode() : 0);
-        // Return
-        return (hash);
-        // End Return
-
-    }
-    
-    public static void main(String[] args) {
-    }
     protected boolean JRcalled = false;
     protected JRItem jrresref;
     public Object JRgetjrresref()
